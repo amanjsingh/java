390f942820625a5a9777422afd4a84503e31256e
amanjsingh <amanjsingh005@gmail.com>
2023-01-16 11:06:35 -0400

    LocalDate, LocalTime and LocalDateTime

---
+++ Java8/src/DateAndTime.java
@@ -0,0 +1,39 @@
+import java.time.*;
+
+public class DateAndTime {
+    public static void main(String[] args) {
+//       LocalDate
+        LocalDate localDate1 = LocalDate.now();
+        LocalDate localDate2 = LocalDate.of(1994,11,3);
+        LocalDate localDate3 = LocalDate.of(2018, Month.JULY, 4);
+        System.out.println(localDate1);
+        System.out.println(localDate2);
+        System.out.println(localDate3);
+
+//       LocalTime
+        LocalTime localTime1 = LocalTime.now();
+        LocalTime localTime2 = LocalTime.of(22, 51);
+        System.out.println(localTime1);
+        System.out.println(localTime2);
+
+//       LocalDateTime
+        LocalDateTime localDateTime1 = LocalDateTime.now();
+        LocalDateTime localDateTime2 = LocalDateTime.of(2018,7,4,8,0);
+        LocalDateTime localDateTime3 = LocalDateTime.of(localDate1, localTime1);
+        System.out.println(localDateTime1);
+        System.out.println(localDateTime2);
+        System.out.println(localDateTime3);
+
+//        MonthDay
+        MonthDay monthDay = MonthDay.of(11,13);
+        LocalDate localDate4 = monthDay.atYear(1994);
+        System.out.println(monthDay);
+        System.out.println(localDate4);
+
+//        YearMonth
+        YearMonth yearMonth = YearMonth.of(2018, Month.JULY);
+        LocalDate localDate5 = yearMonth.atDay(4);
+        System.out.println(yearMonth);
+        System.out.println(localDate5);
+    }
+}
